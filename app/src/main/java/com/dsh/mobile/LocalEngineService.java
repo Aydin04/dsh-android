07ff64b32bd8efdad64f03d7210b84b1c3477d8f
Aydin04 <aydin@example.com>
2026-08-31 18:56:00 +0000

    fix(imports): add Collections, ArrayList, and SequenceInputStream imports

--- app/src/main/java/com/dsh/mobile/LocalEngineService.java
+++ app/src/main/java/com/dsh/mobile/LocalEngineService.java
@@ -25,9 +25,12 @@ import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.SequenceInputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class LocalEngineService extends Service {
