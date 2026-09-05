e63b410d8dc4cc407f5613cb942a112a513f48ad
Ubuntu <aydin@vps.yqvymdej0bxe3kyeedb0zungue.px.internal.cloudapp.net>
2026-09-05 18:40:22 +0000

    fix(build): add missing FileInputStream import in LocalEngineService

--- app/src/main/java/com/dsh/mobile/LocalEngineService.java
+++ app/src/main/java/com/dsh/mobile/LocalEngineService.java
@@ -20,6 +20,7 @@ import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.InputStream;
